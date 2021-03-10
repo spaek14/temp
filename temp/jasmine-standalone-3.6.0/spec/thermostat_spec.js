8b65c6cd2a56bf6a290c687fcb5ab8676659cf72
spaek14 <>
2021-03-10 17:35:42 +0000

    2nd test

--- temp/jasmine-standalone-3.6.0/spec/thermostat_spec.js
+++ temp/jasmine-standalone-3.6.0/spec/thermostat_spec.js
@@ -5,4 +5,12 @@ describe("Thermostat", function() {
       expect(temp.temperature()).toBe(20);
     });
   });
+
+  describe(".up", function() {
+    it("you can increase the temperature with an up function", function() {
+      var thermo = new Thermostat;
+      thermo.up(5);
+      expect(temp.temperature()).toBe(25);
+    });
+  });
 });
